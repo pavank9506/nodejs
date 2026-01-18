5d6536d4bf3a4afee68deea6f7ee677eb0084c39
Your Name <pavan.kota439@email.com>
2026-01-18 10:04:31 +0530

    chart with v1

--- jest.setup.integration.js
+++ jest.setup.integration.js
@@ -2,7 +2,7 @@ let server;
 
 beforeAll((done) => {
   // Start your Node.js server before running integration tests
-  const app = require('./server'); // Update with your actual entry file
+  const app = require('./index');
   server = app.listen(3000, () => {
     console.log('Test server running on port 3000');
     done();
